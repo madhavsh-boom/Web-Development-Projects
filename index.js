2ea1eec34ca4640c01be4155d06b11cf033499aa
madhavsh-boom <madhavsh313@gmail.com>
2025-06-16 22:09:38 +0530

    done for now

---
+++ index.js
@@ -0,0 +1,14 @@
+// async function getData() {
+//     try {
+//         const res = await fetch('https://api.wheretheiss.at/v1/satellites/25544');
+//         const data = await res.json();
+//         console.log(data)
+//         // console.log(`Longitude: ${data.longitude.toFixed(5)}`);
+//         // console.log(`Latitude: ${data.latitude.toFixed(5)}`);
+//         // console.log(`Velocity: ${data.velocity.toFixed(2)} km/h`);
+//     } catch (err) {
+//         console.error('Error fetching ISS data:', err);
+//     }
+// }
+
+// getData();
